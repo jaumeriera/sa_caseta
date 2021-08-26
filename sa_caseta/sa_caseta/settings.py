9af2bbce56a7a343da7f6cfe79e941253e6312df
Jaume Riera Genovart <jaumerg95@gmail.com>
2021-08-26 18:33:24 +0200

    Add new app to installed apps

--- sa_caseta/sa_caseta/settings.py
+++ sa_caseta/sa_caseta/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+OWN_APPS = [
+    'seed',
+]
+
+INSTALLED_APPS = BASE_APPS + OWN_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
