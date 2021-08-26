2641ff661ec0ed8a2a50c523572b7306ea10fdd0
Jaume Riera Genovart <jaumerg95@gmail.com>
2021-08-26 19:29:55 +0200

    Add admin for seed

--- sa_caseta/seed/admin.py
+++ sa_caseta/seed/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from seed.models import Seed
 
-# Register your models here.
+class SeedAdmin(admin.ModelAdmin):
+    exclude = ('uniqueId',)
+    filter_horizontal = ('auxiliar_seeds',)
+admin.site.register(Seed, SeedAdmin)
